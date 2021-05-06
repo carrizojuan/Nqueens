c94161b281e98b2403bfe785dffc0f5b93d6fc9b
Juan Cruz Carrizo <68020794+juancrucarrizo@users.noreply.github.com>
2021-05-06 18:53:51 -0300

    Add files via upload

---
+++ Nqueens2.py
@@ -0,0 +1,34 @@
+
+
+def buscariguales(filas,valor): 
+    c=0
+    for valores in filas:
+        if valores == valor: 
+            c =c+1
+    return c
+
+def validarFila(filas):
+    for i in range (len (filas)):
+            c = buscariguales(filas,filas[i])
+            if c>1: 
+                return False
+    return True
+
+def validarDiagonal(posreinas):
+    diagonalocupadas = []
+    for i in range(len(posreinas)):
+        if ((posreinas[i] - (i+1)) and (posreinas[i] + i + 1)) in diagonalocupadas:
+            return False
+        else:
+            diagonalocupadas.append(posreinas[i]-(i+1))
+            diagonalocupadas.append(posreinas[i]+i+1)
+    return True
+
+def validarReinas(posreinas):
+    return (validarFila(posreinas)  and validarDiagonal(posreinas))
+
+print (validarReinas([4, 2, 7, 3, 6, 8, 5, 1]))
+print (validarReinas([2, 5, 7, 4, 1, 8, 6, 3]))
+print (validarReinas([5, 3, 1, 4, 2, 8, 6, 3]))
+print (validarReinas([5, 8, 2, 4, 7, 1, 3, 6]))
+print (validarReinas([4, 3, 1, 8, 1, 3, 5, 2]))
